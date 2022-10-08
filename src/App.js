3578350f9a7fce7df096ab101d588d3b25aabbba
FIRST_SHIKHA LAST_RAI <raishikha114@gmail.com>
2022-10-07 22:52:00 -0400

    shikha88

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Instakart</a>
+      </header>
+      <main>product list</main>
+    </div>
+  );
+}
+
+export default App;
